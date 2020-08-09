3800c033d4e60c13c08a623851f3e92f92f83f10
vdsalex <alexandre.vandensteen@gmail.com>
2020-08-09 17:36:10 +0200

    adding materialize.css in index

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'sw-ng9-app';
+
+  constructor(private titleService: Title) {
+    titleService.setTitle("7 Wonders");
+  }
 }
