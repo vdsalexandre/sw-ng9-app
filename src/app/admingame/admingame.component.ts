881f38cf76018c76c747857038882b46f5a00926
vdsalex <alexandre.vandensteen@gmail.com>
2020-09-20 18:14:42 +0200

    adding jquery lib, updating admingame component, create background image transition

--- src/app/admingame/admingame.component.ts
+++ src/app/admingame/admingame.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as $ from 'jquery';
+
 @Component({
   selector: 'app-admingame',
   templateUrl: './admingame.component.html',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdmingameComponent implements OnInit {
 
+  private delayChangeInMs: number = 10000;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.updateBackgroundImage();
   }
 
+  updateBackgroundImage(): void {
+    const images: string[] = ['babylone', 'alexandrie', 'artemis', 'gizeh', 'halicarnasse', 'rhodes', 'zeus'];
+    let index: number = 0;
+
+    $("#divAdmin").css("background-image", "url('../../assets/img/empty_cities/" + images[index] + ".jpg')");
+
+    setInterval(function() {
+      index++;
+      if (index == images.length) index = 0;
+
+      $("#divAdmin").css("background-image", "url('../../assets/img/empty_cities/" + images[index] + ".jpg')");
+    }, this.delayChangeInMs);
+  }
 }
